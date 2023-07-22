abe49987d4ef46fbc85f1414b6545e8918db00c7
MeetPitsy <130001000+MeetPitsy@users.noreply.github.com>
2023-07-21 20:41:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import replicate
 import os
 
-# App title
+# PITSY CHATBOT
 st.set_page_config(page_title="💬 PitsyBot")
 
 # Replicate Credentials
@@ -10,15 +10,15 @@ with st.sidebar:
     st.title('💬 PitsyBot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        replicate_api_token = st.secrets['REPLICATE_API_TOKEN']
     else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        replicate_api_token = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
     st.markdown('📖 Learn how to build this app in this [blog](#link-to-blog)!')
-os.environ['REPLICATE_API_TOKEN'] = replicate_api
+os.environ['REPLICATE_API_TOKEN'] = str(replicate_api)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
